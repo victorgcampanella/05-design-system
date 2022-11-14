d1d83e1dc1dcbf9ef526e225d43730e3ab1f2842
Victor Galvão Campanella <vgcgalvao@outlook.com>
2022-11-13 21:21:08 -0300

    ci: Add TurboRepo cache on deploy

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@ds-rocket-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
